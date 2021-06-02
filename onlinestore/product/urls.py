9928659a09063d7fa749583f513f19a9bcb1d338
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-06-02 20:33:57 +0530

    first project done

--- onlinestore/product/urls.py
+++ onlinestore/product/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, product_detail, manufacturer_list, manufacturer_detail
 
 urlpatterns = [
     path("products/", product_list, name='product-list'),
+    path("products/<int:pk>/", product_detail, name='product-detail'),
+    path("manufacturers/", manufacturer_list, name='manufacturer_list'),
+    path("manufacturers/<int:pk>/", manufacturer_detail,
+         name='manufacturer_detail'),
 ]
