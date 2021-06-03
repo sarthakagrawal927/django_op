25e075a62daba2d4bb358cf014a7ac18c0724143
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-06-03 13:55:12 +0530

    s

--- onlinestore/product/views.py
+++ onlinestore/product/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 
 from .models import Student
 
@@ -10,17 +11,16 @@ def student_detail(request, registrationNo):
             "name": student.name,
             "registrationNo": student.registrationNo,
             "cgpa": student.cgpa}}
-        response = JsonResponse(data)
 
     except Student.DoesNotExist:
-        response = JsonResponse({"error": {
-            "code": 404, "message": "not found"
-        }}, status=404)
-    return response
+        data = "not found"
+
+    return render(request, "student.html", {'data': data})
+    # return response
 
 
 def student_list(request):
     students = Student.objects.all()
     data = {"students": list(students.values())}
     response = JsonResponse(data)
-    return response
+    return render(request, "students.html", {'data': data})
